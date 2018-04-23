20e295523676583c5a993d637a90bd347d3514e1
beatorizu <betrizaf@gmail.com>
2018-04-22 22:21:27 -0300

    Adicionado método genérico para espera explícita
    Para casos em que há atualização na página, essa espera dará um tempo
    para o navegador renderizar a página e o Selenium pode procurar o
    elemento.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -8,16 +8,33 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Edith acessa a página inicial e acidentalmente tenta submeter
         # um item vazio na lista. Ela tecla Enter na caixa de entrada vazia
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # A página inicial é atualizada e há uma mensagem de erro informando
         # que itens da lista não podem estar em branco
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
 
-        # Eis que ela tenta novamente com um texto para o item, e isso functional_tests
+        # Eis que ela tenta novamente com um texto para o item, e isso agora funciona
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
 
         # De formar pervesa, ela decide submeter um segundo item em
         # branco na listas
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # Ela recebe um aviso semelhante na página da listas
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
 
         # Ela pode corrigir isso preenchendo o item com um texto
-        self.fail('write me plz!')
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
